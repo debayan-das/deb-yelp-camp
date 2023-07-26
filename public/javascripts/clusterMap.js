31d86511ca4b3d8317c67249a257ec0172d26933
Debayan Das <67179498+debayan-das@users.noreply.github.com>
2023-07-26 13:27:38 +0530

    Update cluster Map center location

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
 	container: "cluster-map",
 	style: "mapbox://styles/mapbox/light-v10",
-	center: [-103.59179687498357, 40.66995747013945],
+	center: [79.1219684394686, 22.93342571169821],
 	zoom: 3,
 });
 
